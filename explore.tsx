75862c2e8f3decb7b756eaed12075d46904b584e
jennyferparmar16-yel <jenniparmar7@gmail.com>
2025-12-15 00:05:46 -0500

    _layout.tsx, explore.tsx

---
+++ explore.tsx
@@ -0,0 +1,16 @@
+// Course: MAD201 – Project 2
+// Name: Jennyfer Parmar
+// Student ID: A00201240
+// Date: 12/12/25
+// Description: Template screen kept for compatibility; hidden from tabs.
+
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function ExploreScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Explore</Text>
+    </View>
+  );
+}
